42cf7259294649763baff6fbeb87e43962181e46
Dominic Tremblay <dominictremblay24@gmail.com>
2023-07-25 12:34:07 -0400

    final version of the sum problem

--- sum.js
+++ sum.js
@@ -2,10 +2,15 @@
 
 // extract the command-line arguments
 const args = process.argv.slice(2);
-console.log("Args:", args);
+console.log('Args:', args);
 
-// edge case: We need at least 2 arguments.
-
+// edge case: We need at least 2 arguments, if not output an error message
+if (args.length < 2) {
+  console.log('Error: please enter at least 2 arguments');
+  // stop the execution here
+  // return; // it works!!
+  process.exit();
+}
 
 // going through each argument
 // for loop
@@ -16,20 +21,22 @@ console.log("Args:", args);
 let total = 0;
 
 for (let arg of args) {
-
-  
   // add them up
-  // typecasting
-  total += Number(arg);
-  console.log("args:", arg, "total:", total);
+  // edge case: If any argument is not a whole number, skip it.
 
+  const convertedNum = Number(arg);
+
+  if (Number.isInteger(convertedNum)) {
+    // typecasting
+    total += convertedNum;
+  }
+  console.log('args:', arg, 'total:', total);
 }
-
-// edge case: If any argument is not a whole number, skip it.
-
 // edge case: If any argument is not a number, output an error message.
-
+if (isNaN(convertedNum)) {
+  console.log('Error: please enter only numbers');
+  process.exit();
+} 
 
 // print the sum of them
-console.log("Total:", total);
-
+console.log('Total:', total);
